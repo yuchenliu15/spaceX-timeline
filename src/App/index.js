6db9174042794064aa0dd9719ec1138ded4c7331
Yuchen Liu <yl6448@nyu.edu>
2020-05-27 13:55:39 -0700

    fix Timer function

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { Timer } from '../Timer';
 
+const spacex = `https://api.spacexdata.com/v3/launches/94`;
+
+const getData = async () => {
+  return fetch(spacex)
+          .then(res => res.json())
+}
+
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  useEffect(() => {
+    (async () => {
+      setData(await getData());
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <Timer />
+      {data? <Timer goal={data['launch_date_utc']} />:<p>Wait a sec</p>}
     </div>
   );
 }
--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,35 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
-const spacex = `https://api.spacexdata.com/v3/launches/94`;
-
-const current = new Date("2020-05-27T20:33:00.000Z");
-
-// const getLaunchTime = async () => {
-//     return fetch(spacex)
-//             .then(res => res.json())
-//             .then(data => new Date(data['launch_date_utc']));
-// }
-
-const getDiff = (goal) => () => {
+const getDiff = (goal) => {
     if (goal)
         return new Date(goal - new Date());
 }
 
-const Timer = () => {
+const Timer = ({ goal }) => {
 
-    const [time, setTime] = React.useState(null);
+    const goalDate = new Date(goal);
     const [countdown, setCoundown] = React.useState(null);
-    const timeLeft = countdown? countdown.getHours() + ":" + countdown.getMinutes() + ":" + countdown.getSeconds(): "";
+    //const timeLeft = countdown.getHours() + ":" + countdown.getMinutes() + ":" + countdown.getSeconds();
 
     useEffect(() => {
-
-        setTimeout(()=>setCoundown(getDiff(current)), 1000);
-
+        setTimeout(() => setCoundown(getDiff(goalDate)), 1000);
     });
 
     return (
         <div>
-            <h1>{timeLeft}</h1>
+            {countdown? <h1>{countdown.getDay() + ":" + countdown.getHours() + ":" + countdown.getMinutes() + ":" + countdown.getSeconds()}</h1>: <h1>Loading</h1>}
         </div>
     )
 
