753266352639ed5636e12764115c0fea820d2fac
Yuchen Liu <yl6448@nyu.edu>
2020-05-27 13:04:00 -0700

    add timer

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -2,25 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 const spacex = `https://api.spacexdata.com/v3/launches/94`;
 
-const getLaunchTime = async () => {
-    return fetch(spacex)
-            .then(res => res.json())
-            .then(data => new Date(data['launch_date_utc']));
+// const getLaunchTime = async () => {
+//     return fetch(spacex)
+//             .then(res => res.json())
+//             .then(data => new Date(data['launch_date_utc']));
+// }
+
+const getDiff = (goal) => () => {
+    console.log('heheh')
+    if (goal)
+        return new Date(goal - new Date());
 }
 
 const Timer = () => {
-    
+
     const [time, setTime] = React.useState(null);
+    const [countdown, setCoundown] = React.useState(null);
 
     useEffect(() => {
-        (async () => {
-            setTime(await getLaunchTime())
-        })();
-    }, []);
+
+        setTimeout(()=>setCoundown(new Date("2020-05-27T20:33:00.000Z").getSeconds() - new Date().getSeconds()), 1000);
+
+    });
 
     return (
         <div>
-        {time? <p>{time.toString()}</p>: <p>NO</p>}
+            {countdown ? <p>{countdown}</p> : <p>NO</p>}
         </div>
     )
 
