dbf7810627b213086fd9d340a0aed8fa6531ae26
Yuchen Liu <yl6448@nyu.edu>
2020-05-27 12:13:02 -0700

    add api

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 const spacex = `https://api.spacexdata.com/v3/launches/94`;
 
+const getLaunchTime = async () => {
+    return fetch(spacex)
+            .then(res => res.json())
+            .then(data => new Date(data['launch_date_utc']));
+}
+
 const Timer = () => {
     
     const [time, setTime] = React.useState(null);
 
     useEffect(() => {
-        fetch(spacex)
-            .then(res => res.json())
-            .then(data => {
-                setTime(data.launch_year);
-            })
+        (async () => {
+            setTime(await getLaunchTime())
+        })();
     }, []);
 
     return (
