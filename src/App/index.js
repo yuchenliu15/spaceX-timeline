71730d971bd5b4c18fb0f931cc1703fa2bd620f1
Yuchen Liu <yl6448@nyu.edu>
2020-05-31 10:07:41 -0700

    add search

--- src/App/index.js
+++ src/App/index.js
@@ -9,7 +9,7 @@ import { Menu } from '../Menu';
 
 const PATH_BASE = 'https://api.spacexdata.com/v3';
 const LAUNCH_UPCOMING = '/launches/upcoming';
-const spacex = `${PATH_BASE}${LAUNCH_UPCOMING}`;
+const DEFAULT_URL = `${PATH_BASE}${LAUNCH_UPCOMING}`;
 
 const Sorts = {
   latest: list => sortBy(list, 'date'),
@@ -17,12 +17,26 @@ const Sorts = {
   name: list => sortBy(list, 'title')
 }
 
+const withLength = (Component) => ({ list, ...props }) => {
+
+  if(Array.isArray(list) && list.length > 0) {
+    return list.map((item, index) => <Component key={index} number={index} title={item['name']} date={item['date']} {...props} />)
+  }
+  else if (!Array.isArray(list) && typeof list === 'object' && list !== null) {
+    return <Component className="single" title={list['name']} date={list['date']} {...props} />
+  }
+  else {
+    return <span className="spinner-grow" role="status" ></span>
+  }
+
+}
+
 const getData = async (url) => {
   return fetch(url).then(res => res.json())
 }
 
 const onCardClick = event => {
-  console.log(event.target.getAttribute('number'));
+  console.log(event.target.getAttribute('number')+'haahhahha');
 }
 
 function App() {
@@ -32,7 +46,8 @@ function App() {
   const [activeSort, setActiveSort] = useState('latest');
   const [search, setSearch] = useState('');
 
-  const list = Array.isArray(dataForCard)? Sorts[activeSort](dataForCard): dataForCard;
+  const list = Array.isArray(dataForCard) ? Sorts[activeSort](dataForCard) : dataForCard;
+  const CardWithLength = withLength(Card);
 
   const onSortChange = (event) => {
     const newSort = event.target.text;
@@ -44,33 +59,36 @@ function App() {
   const onSearchChange = event => {
     setSearch(event.target.value)
   }
-  
+
   const onSearchSubmit = () => {
-    updateData(`${PATH_BASE}/launches/${search}`);
+    if(search) {
+      updateData(`${PATH_BASE}/launches/${search}`);
+    } else {
+      updateData(DEFAULT_URL);
+    }
   }
 
   const updateData = async (url) => {
     const res = await getData(url);
-    console.log(res['mission_name'])
     setData(res);
     setDataForCard(
-      Array.isArray(res)?
-      res
-        .filter(item => ((new Date(item['launch_date_local']) - new Date()) > 0))
-        .map(item => ({
-          name: item['mission_name'],
-          date: new Date(item['launch_date_local'])
-        }))
-      : {
-        name: res['mission_name'],
-        date: new Date(res['launch_date_local'])
-      }
+      Array.isArray(res) ?
+        res
+          .filter(item => ((new Date(item['launch_date_local']) - new Date()) > 0))
+          .map(item => ({
+            name: item['mission_name'],
+            date: new Date(item['launch_date_local'])
+          }))
+        : {
+          name: res['mission_name'],
+          date: new Date(res['launch_date_local'])
+        }
     );
 
   }
 
   useEffect(() => {
-    updateData(spacex);
+    updateData(DEFAULT_URL);
   }, []);
 
   return (
@@ -79,10 +97,7 @@ function App() {
       <div className="body-container">
         <Menu sortString={activeSort} onSortChange={onSortChange} onSearchChange={onSearchChange} onSearchSubmit={onSearchSubmit} />
         <div className="card-container">
-          {list.length ?
-            list.map((item, index) => <Card key={index} number={index} title={item['name']} date={item['date']} onCardClick={onCardClick} />)
-            : <span className="spinner-grow" role="status" ></span>
-          }
+          <CardWithLength list={list} onCardClick={onCardClick} />
         </div>
       </div>
 
